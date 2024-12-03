1ea96f27ea9cbc43a796fea928553390155043a6
gunpyo0908 <gunpyo0908@gmail.com>
2024-12-04 00:04:08 +0900

    feat : 처음 시작

---
+++ project1.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    int head = 0;
+    int tail = 0;
+
+    printf("동전의 앞면: %d \n", head);
+    printf("동전의 뒷면: %d \n", tail);
+
+
+    return 0;
+}
